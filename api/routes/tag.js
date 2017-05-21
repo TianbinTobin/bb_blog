f4a93f412731ebcf50e333c71955c5d0621188a0
TianbinTobin <674220619@qq.com>
2017-05-22 00:32:19 +0800

    添加model/tag.js 添加tag_controller

--- api/routes/tag.js
+++ api/routes/tag.js
@@ -8,5 +8,8 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/save', $.createTag);
+router.post('/delete', $.deleteTag);
+router.post('/update', $.modifyTag);
+router.post('/list', $.getAllTags);
 
 module.exports = router;
--- controllers/tag_controller.js
+++ controllers/tag_controller.js
@@ -9,7 +9,7 @@ const createTag = function (req, res) {
     if (name === undefined || name === "") {
         return res.json(result(false, '标签名不能为空'));
     }
-    let query = Tag.findOne({name:name}).exec();
+    let query = Tag.findOne({name:name}).exec().catch(console.error);
     query.then(function (doc) {
         if (doc !== null) {
             return res.json(result(false, '该标签已存在'));
@@ -26,15 +26,47 @@ const createTag = function (req, res) {
 };
 
 const deleteTag = function (req, res) {
-
+    const name = req.body.name;
+    let query = Tag.remove({name: name}).exec().catch(console.error);
+    query.then(function (doc) {
+        if(doc !== null) {
+            return res.json(result(true, '删除成功'));
+        }
+    });
 };
 
 const modifyTag = function (req, res) {
-
+    const _id = req.body._id;
+    const name = req.body.name;
+    let query = Tag.update({_id: _id}, {name: name}).exec().catch(console.error);
+    query.then(function (doc) {
+        if(doc !== null) {
+            return res.json(result(true, '更新成功'));
+        }
+    });
 };
 
 const getAllTags = function (req, res) {
-
+    let query2 = Tag.count().exec().catch(console.err);
+    query2.then(function (total) {
+        let query1 = Tag.find().exec().catch(console.error);
+        query1.then(function (doc) {
+            const data = {};
+            if(doc.length !== 0) {
+                const docArr = [];
+                for (let i = 0, len = doc.length; i < len; i++) {
+                    docArr.push(doc[i].toJSON());
+                }
+                data.data = docArr;
+                data.total = total;
+                return res.json(result(true, data));
+            } else {
+                data.data = "";
+                data.total = total;
+                return res.json(result(true, data));
+            }
+        });
+    });
 };
 
 module.exports = {createTag, deleteTag, modifyTag, getAllTags};
